f0bcf8ba9d8c6cf7541d1ff85cf048e2aaf46417
niteshkumar259 <niteshkumar2599@gmail.com>
2025-05-02 03:48:13 +0500

    update code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React from "react"
-import { Footer } from "./components/footer/Footer"
-import { Header } from "./components/header/Header"
-import { Home } from "./pages/home/Home"
-import { Login } from "./pages/login/Login"
-import { Regsiter } from "./pages/login/Regsiter"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { DetailsPages } from "./pages/details/DetailsPages"
-import { Account } from "./pages/account/Account"
-import { Create } from "./components/create/Create"
+import React from "react";
+import { Footer } from "./components/footer/Footer";
+import { Header } from "./components/header/Header";
+import { Home } from "./pages/home/Home";
+// import { Login } from "./pages/login/Login";
+// import { Register } from "./pages/login/Register"; // Fixed typo
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { DetailsPages } from "./pages/details/DetailsPages";
+// import { Account } from "./pages/account/Account";
+// import { Create } from "./components/create/Create";
 
 const App = () => {
   return (
     <>
       <Router>
         <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Regsiter} />
-          <Route exact path='/details/:id' component={DetailsPages} />
-          <Route exact path='/account' component={Account} />
-          <Route exact path='/create' component={Create} />
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          {/* <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} /> */}
+          <Route exact path="/details/:id" element={<DetailsPages />} />
+          {/* <Route exact path="/account" element={<Account />} />
+          <Route exact path="/create" element={<Create />} /> */}
+        </Routes>
         <Footer />
       </Router>
     </>
-  )
-}
-export default App
+  );
+};
+
+export default App;
